aeeeade21929b4fc88780fcaec6c80ed58611f61
Bryce Klinker <bryce.klinker@gmail.com>
2021-11-02 13:42:51 -0500

    Adding compression plugin

--- node.api.js
+++ node.api.js
@@ -3,7 +3,6 @@ const CompressionPlugin = require("compression-webpack-plugin");
 const BSCSiteReactStaticPlugin = (options) => {
     return {
         webpack: (config, options) => {
-            console.log(config);
             config.plugins.push(new CompressionPlugin());
             return config;
         }
