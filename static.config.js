c470fc9670ba52e9eb1d95c06c10778540760202
Bryce Klinker <bryce.klinker@gmail.com>
2021-11-08 14:40:26 -0600

    Fixing issue in document generation

--- static.config.js
+++ static.config.js
@@ -36,7 +36,7 @@ export default {
                     <link rel="icon" href="./favicon.ico"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'}/>
                     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700;900&display=swap"
                           rel="stylesheet"/>
                     <title>Bondurant Soccer Club</title>
