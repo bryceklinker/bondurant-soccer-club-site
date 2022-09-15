441021e549d22fb08c3d8f59e5f5d76dabee0aa4
Bryce Klinker <bryce.klinker@gmail.com>
2022-09-14 22:38:39 -0500

    using fileset instead of external module

--- infra/modules/static_site/main.tf
+++ infra/modules/static_site/main.tf
@@ -9,75 +9,80 @@ locals {
     "application/json",
     "application/xml"
   ]
+  mime_types = {
+    ".json" = "application/json"
+    ".js" = "application/javascript"
+    ".css" = "text/css"
+    ".html" = "text/html"
+    ".ico" = "image/x-icon"
+    ".png" = "image/png"
+    ".jpg" = "image/jpeg"
+    ".jpeg" = "image/jpeg"
+    ".txt" = "text/plain"
+  }
 }
 
 resource "azurerm_storage_account" "site_storage" {
-  location = var.location
-  name = "st${replace(var.name, "-", "")}"
+  location            = var.location
+  name                = "st${replace(var.name, "-", "")}"
   resource_group_name = var.resource_group_name
 
-  account_tier = "Standard"
-  account_replication_type = "LRS"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
   enable_https_traffic_only = true
 
   static_website {
-    index_document = "index.html"
+    index_document     = "index.html"
     error_404_document = "index.html"
   }
 
   tags = var.tags
 }
 
-module "site_files" {
-  source = "hashicorp/dir/template"
-
-  base_dir = var.site_directory
-}
-
 resource "azurerm_storage_blob" "site_content" {
-  for_each = module.site_files.files
+  for_each = fileset(var.site_directory, "**")
 
-  storage_account_name = azurerm_storage_account.site_storage.name
+  storage_account_name   = azurerm_storage_account.site_storage.name
   storage_container_name = "$web"
 
-  name = each.key
-  source = each.value.source_path
-  content_type = each.value.content_type
-  content_md5 = each.value.digests.md5
-  type = "Block"
+  name         = each.key
+  source       = each.value
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value))
+  content_md5  = filemd5("${var.site_directory}/${each.value}")
+  type         = "Block"
 }
 
 resource "azurerm_cdn_profile" "cdn_profile" {
-  name = "cdnp-${var.name}"
+  name                = "cdnp-${var.name}"
   resource_group_name = var.resource_group_name
-  location = var.location
-  sku = "Standard_Microsoft"
+  location            = var.location
+  sku                 = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "cdn_endpoint" {
-  name = "cdne-${var.name}"
-  profile_name = azurerm_cdn_profile.cdn_profile.name
-  location = azurerm_cdn_profile.cdn_profile.location
-  resource_group_name = var.resource_group_name
-  is_compression_enabled = true
-  is_http_allowed = true
-  optimization_type = "GeneralWebDelivery"
+  name                      = "cdne-${var.name}"
+  profile_name              = azurerm_cdn_profile.cdn_profile.name
+  location                  = azurerm_cdn_profile.cdn_profile.location
+  resource_group_name       = var.resource_group_name
+  is_compression_enabled    = true
+  is_http_allowed           = true
+  optimization_type         = "GeneralWebDelivery"
   content_types_to_compress = local.content_types_to_compress
-  origin_host_header = azurerm_storage_account.site_storage.primary_web_host
+  origin_host_header        = azurerm_storage_account.site_storage.primary_web_host
 
   delivery_rule {
-    name = "HTTPSRedirect"
+    name  = "HTTPSRedirect"
     order = 1
 
     request_scheme_condition {
-      operator = "Equal"
+      operator         = "Equal"
       negate_condition = false
-      match_values = ["HTTP"]
+      match_values     = ["HTTP"]
     }
 
     url_redirect_action {
       redirect_type = "Found"
-      protocol = "Https"
+      protocol      = "Https"
     }
   }
 
@@ -92,7 +97,7 @@ resource "azurerm_dns_cname_record" "cname" {
   resource_group_name = var.dns_resource_group_name
   zone_name           = var.dns_zone_name
   ttl                 = 3600
-  target_resource_id = azurerm_cdn_endpoint.cdn_endpoint.id
+  target_resource_id  = azurerm_cdn_endpoint.cdn_endpoint.id
 }
 
 resource "azurerm_cdn_endpoint_custom_domain" "custom_domain" {
@@ -110,15 +115,15 @@ resource "azurerm_log_analytics_workspace" "log_workspace" {
   location            = var.location
   name                = "log-${var.name}"
   resource_group_name = var.resource_group_name
-  daily_quota_gb = 0.5
+  daily_quota_gb      = 0.5
 }
 
 resource "azurerm_application_insights" "web_app_insights" {
-  location = var.location
-  name = "appi-${var.name}"
+  location            = var.location
+  name                = "appi-${var.name}"
   resource_group_name = var.resource_group_name
-  application_type = "other"
-  workspace_id = azurerm_log_analytics_workspace.log_workspace.id
+  application_type    = "other"
+  workspace_id        = azurerm_log_analytics_workspace.log_workspace.id
 
   tags = var.tags
 }
