53bffadb2b028bf2e064f3b6396a9f5d642b115d
Bryce Klinker <bryce.klinker@gmail.com>
2022-09-14 22:07:11 -0500

    pinning azurerm version

--- infra/modules/static_site/provider.tf
+++ infra/modules/static_site/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.22.0"
+      version = "=3.22.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
