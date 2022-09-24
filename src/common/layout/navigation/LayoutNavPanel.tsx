c32f1f32bfc05b2bb85e2598d1481f77c439b7a7
Bryce Klinker <bryce.klinker@gmail.com>
2022-09-24 09:50:17 -0500

    navpanel seems to be working correctly

--- src/common/layout/navigation/LayoutNavPanel.tsx
+++ src/common/layout/navigation/LayoutNavPanel.tsx
@@ -1,23 +1,55 @@
-import { FC } from 'react';
-import { Drawer } from '@chakra-ui/react';
+import { FC, useRef } from 'react';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerContent,
+    DrawerOverlay,
+    IconButton
+} from '@chakra-ui/react';
 import { ColumnFlex } from '../ColumnFlex';
-import { MainNavigationLinks } from './MainNavigationLinks';
-import { MoreNavigationLinks } from './MoreNavigationLinks';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
+import './LayoutNavPanel.css';
+import {
+    MAIN_NAVIGATION_LINKS,
+    MORE_INFO_NAVIGATION_LINKS
+} from '../../routing/route-names';
+import { NavLink } from './NavLink';
 export type LayoutNavPanelProps = {
     isOpen: boolean;
+    onOpen: () => void;
     onClose: () => void;
 };
 export const LayoutNavPanel: FC<LayoutNavPanelProps> = ({
     isOpen,
-    onClose
+    onClose,
+    onOpen
 }) => {
+    const buttonRef = useRef();
+    const links = MAIN_NAVIGATION_LINKS.concat(MORE_INFO_NAVIGATION_LINKS).map(
+        (link, index) => <NavLink data={link} onClick={onClose} />
+    );
     return (
-        <Drawer isOpen={isOpen} onClose={onClose}>
-            <ColumnFlex>
-                <MainNavigationLinks onClick={onClose} />
-                <MoreNavigationLinks onClick={onClose} />
-            </ColumnFlex>
-        </Drawer>
+        <>
+            <IconButton
+                ref={buttonRef}
+                colorScheme={'black'}
+                onClick={onOpen}
+                aria-label={'menu'}
+                icon={<HamburgerIcon />}
+            />
+            <Drawer
+                placement={'left'}
+                isOpen={isOpen}
+                onClose={onClose}
+                finalFocusRef={buttonRef}>
+                <DrawerOverlay />
+                <DrawerContent className={'navpanel'}>
+                    <DrawerBody>
+                        <ColumnFlex>{links}</ColumnFlex>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </>
     );
 };
--- src/common/layout/navigation/LayoutNavbar.tsx
+++ src/common/layout/navigation/LayoutNavbar.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+import { useMediaQuery } from '@chakra-ui/react';
 import { RowFlex } from '../RowFlex';
 import { MainNavigationLinks } from './MainNavigationLinks';
 import { MoreNavigationLinks } from './MoreNavigationLinks';
@@ -6,15 +7,24 @@ import { LayoutNavPanel } from './LayoutNavPanel';
 import { useBooleanToggle } from '../../hooks/use-boolean-toggle';
 
 import './LayoutNavbar.css';
+
 export type LayoutNavbarProps = {};
 export const LayoutNavbar: FC<LayoutNavbarProps> = () => {
     const { value, toggleOn, toggleOff } = useBooleanToggle();
+    const [isSmallScreen] = useMediaQuery('(max-width: 768px)');
+    const isLargeScreen = useMemo(() => !isSmallScreen, [isSmallScreen]);
     return (
         <>
-            <LayoutNavPanel isOpen={value} onClose={toggleOff} />
             <RowFlex flex={0} as={'nav'} className={'navbar'}>
-                <MainNavigationLinks onClick={toggleOff} />
-                <MoreNavigationLinks onClick={toggleOff} />
+                {isSmallScreen && (
+                    <LayoutNavPanel
+                        isOpen={value}
+                        onOpen={toggleOn}
+                        onClose={toggleOff}
+                    />
+                )}
+                {isLargeScreen && <MainNavigationLinks onClick={toggleOff} />}
+                {isLargeScreen && <MoreNavigationLinks onClick={toggleOff} />}
             </RowFlex>
         </>
     );
