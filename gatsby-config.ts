5c867df965167bcdf3e8b3600224fa0b9d02832c
Bryce Klinker <bryce.klinker@gmail.com>
2022-09-30 10:13:03 -0500

    adding web vitals and google analytics to site

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,6 +24,29 @@ const config: GatsbyConfig = {
         'gatsby-plugin-robots-txt',
         'gatsby-plugin-postcss',
         {
+            resolve: 'gatsby-plugin-web-vitals',
+            options: {
+                metrics: ['FID', 'TTFB', 'LCP', 'CLS', 'FCP'],
+                eventCategory: 'Performance',
+                includeInDevelopment: false,
+                debug: false
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-google-gtag',
+            options: {
+                trackingIds: [process.env.GOOGLE_MEASUREMENT_ID || ''],
+                gtagConfig: {
+                    anonymize_ip: true,
+                    cookie_expires: 0
+                },
+                pluginConfig: {
+                    head: false,
+                    respectDNT: true
+                }
+            }
+        },
+        {
             resolve: 'gatsby-plugin-manifest',
             options: {
                 name: 'Bondurant Soccer Club',
