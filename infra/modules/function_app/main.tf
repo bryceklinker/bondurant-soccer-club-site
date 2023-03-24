071f5a007b9b95c34cf20a5419d8c4fe95896d89
Bryce Klinker <bryce.klinker@gmail.com>
2023-03-23 20:06:59 -0500

    adjusting package json for running on ci

--- infra/modules/function_app/main.tf
+++ infra/modules/function_app/main.tf
@@ -38,9 +38,9 @@ resource "azurerm_linux_function_app" "app" {
   storage_account_name       = var.storage_account_name
   service_plan_id            = azurerm_service_plan.plan.id
 
-  name    = "func-${var.name}"
-  os_type = "linux"
-  version = "~4"
+  name       = "func-${var.name}"
+  os_type    = "linux"
+  version    = "~4"
   https_only = true
 
   identity {
@@ -50,16 +50,23 @@ resource "azurerm_linux_function_app" "app" {
   site_config {
 
     APPLICATIONINSIGHTS_CONNECTION_STRING = var.application_insights_connection_string
-    STORAGE_ACCOUNT_CONNECTION_STRING = var.storage_account_connection_string
-    ALERTS_QUEUE_NAME = azurerm_storage_queue.alerts_queue.name
-    SITE_CONTAINER_NAME = var.storage_account_web_container
-    WEBSITE_RUN_FROM_PACKAGE = azurerm_storage_blob.function_app.url
-    DB_BLOB_PREFIX = "db"
+    STORAGE_ACCOUNT_CONNECTION_STRING     = var.storage_account_connection_string
+    ALERTS_QUEUE_NAME                     = azurerm_storage_queue.alerts_queue.name
+    SITE_CONTAINER_NAME                   = var.storage_account_web_container
+    WEBSITE_RUN_FROM_PACKAGE              = azurerm_storage_blob.function_app.url
+    DB_BLOB_PREFIX                        = "db"
+    FUNCTIONS_WORKER_RUNTIME              = "node"
   }
 }
 
 resource "azurerm_role_assignment" "blob_contributor" {
   principal_id         = azurerm_linux_function_app.app.identity.0.principal_id
   role_definition_name = "Storage Blob Data Contributor"
+  scope                = var.storage_account_id
+}
+
+resource "azurerm_role_assignment" "queue_contributor" {
+  principal_id         = azurerm_linux_function_app.app.identity.0.principal_id
+  role_definition_name = "Storage Queue Data Contributor"
   scope                = var.storage_account_id
 }
