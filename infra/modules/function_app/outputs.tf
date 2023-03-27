511baa54567a1c14b526d1045fbe283af1b196c4
Bryce Klinker <bryce.klinker@gmail.com>
2023-03-27 15:36:30 -0500

    more issues being cleared up

--- infra/modules/function_app/outputs.tf
+++ infra/modules/function_app/outputs.tf
@@ -1,7 +1,7 @@
 output "function_app_url" {
-  value = "https://${azurerm_linux_function_app.app.default_hostname}"
+  value = "https://${azurerm_windows_function_app.app.default_hostname}"
 }
 
 output "function_app_name" {
-  value = azurerm_linux_function_app.app.name
+  value = azurerm_windows_function_app.app.name
 }
