5f9fbddb256a6fb01e73641c474213a62c8d47f6
Bryce Klinker <bryce.klinker@gmail.com>
2023-03-30 16:27:35 -0500

    making tailwind config more broad

--- packages/site/tailwind.config.js
+++ packages/site/tailwind.config.js
@@ -2,9 +2,7 @@
 
 module.exports = {
     content: [
-        './src/common/components/**/*.{js,jsx,ts,tsx}',
-        './src/common/layout/**/*.{js,jsx,ts,tsx}',
-        './src/common/seo/**/*.{js,jsx,ts,tsx}',
+        './src/common/**/*.{js,jsx,ts,tsx}',
         './src/components/**/*.{js,jsx,ts,tsx}',
         './src/pages/**/*.{js,jsx,ts,tsx}'
     ],
