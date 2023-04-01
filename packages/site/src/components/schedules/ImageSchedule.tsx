d17a3006a97d58980e250d6310a02171786f1a43
Bryce Klinker <bryce.klinker@gmail.com>
2023-04-01 09:26:09 -0500

    fixing style issues with new images

--- packages/site/src/components/schedules/ImageSchedule.tsx
+++ packages/site/src/components/schedules/ImageSchedule.tsx
@@ -1,20 +1,23 @@
-import { FunctionComponent, PropsWithChildren } from 'react';
-import { Section } from '../../common/components/Section';
-import { CollapsiblePanel } from '../../common/components/CollapsiblePanel';
-import { SectionTitle } from '../../common/components/SectionTitle';
+import {FunctionComponent, PropsWithChildren} from 'react';
+import {Section} from '../../common/components/Section';
+import {CollapsiblePanel} from '../../common/components/CollapsiblePanel';
+import {SectionTitle} from '../../common/components/SectionTitle';
+import {RowFlex} from '../../common/layout/RowFlex';
 
 export type ImageScheduleProps = PropsWithChildren & {
     title: string;
 };
 
 export const ImageSchedule: FunctionComponent<ImageScheduleProps> = ({
-    title,
-    children
-}) => {
+                                                                         title,
+                                                                         children
+                                                                     }) => {
     return (
         <Section padded shadow>
             <CollapsiblePanel title={<SectionTitle>{title}</SectionTitle>}>
-                {children}
+                <RowFlex>
+                    {children}
+                </RowFlex>
             </CollapsiblePanel>
         </Section>
     );
