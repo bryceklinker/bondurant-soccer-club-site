bc00f974fdfa26b283dadf22873490249af8192e
Bryce Klinker <bryce.klinker@gmail.com>
2023-06-07 21:53:23 -0500

    upgrading packages. adding function app back to e2e

--- packages/site/src/testing/model-factory.ts
+++ packages/site/src/testing/model-factory.ts
@@ -10,13 +10,13 @@ import { SiteMetadata } from '../common/hooks/useSiteMetadata';
 
 function boardMember(model: Partial<BoardMemberData> = {}): BoardMemberData {
     return {
-        roleTitle: faker.name.jobTitle(),
+        roleTitle: faker.person.jobTitle(),
         role: faker.helpers.arrayElement(Object.values(BoardMemberRole)),
         email: faker.internet.email(),
         roleGroup: faker.helpers.arrayElement(
             Object.values(BoardMemberRoleGroup)
         ),
-        name: faker.name.firstName(),
+        name: faker.person.firstName(),
         ...model
     };
 }
@@ -25,7 +25,7 @@ function alert(model: Partial<AlertModel> = {}): AlertModel {
     return {
         severity: faker.helpers.arrayElement(Object.values(AlertSeverity)),
         text: faker.lorem.sentence(),
-        id: faker.datatype.uuid(),
+        id: faker.string.uuid(),
         startDate: faker.date.recent().toISOString(),
         ...model
     };
@@ -41,12 +41,12 @@ function settings(model: Partial<SettingsModel> = {}): SettingsModel {
 function user(user: Partial<AuthUser> = {}): AuthUser {
     return {
         email: faker.internet.email(),
-        name: faker.name.fullName(),
-        exp: faker.date.soon(2).getTime() / 1000,
-        family_name: faker.name.lastName(),
-        iat: faker.date.recent(1).getTime() / 1000,
-        given_name: faker.name.firstName(),
-        sub: faker.datatype.uuid(),
+        name: faker.person.fullName(),
+        exp: faker.date.future({years: 2}).getTime() / 1000,
+        family_name: faker.person.lastName(),
+        iat: faker.date.recent({days: 1}).getTime() / 1000,
+        given_name: faker.person.firstName(),
+        sub: faker.string.uuid(),
         picture: faker.internet.url(),
         hd: faker.internet.domainName(),
         ...user
@@ -56,7 +56,7 @@ function user(user: Partial<AuthUser> = {}): AuthUser {
 function jwtForUser(user: Partial<AuthUser> = {}): string {
     const header = {
         alg: 'RS256',
-        kid: faker.datatype.uuid().replace(/-/g, ''),
+        kid: faker.string.uuid().replace(/-/g, ''),
         typ: 'JWT'
     };
     const payload = ModelFactory.user(user);
@@ -71,16 +71,16 @@ function credentialsForUser(user: Partial<AuthUser> = {}): CredentialResponse {
     return {
         credential: ModelFactory.jwtForUser(user),
         select_by: 'auto',
-        clientId: faker.datatype.uuid()
+        clientId: faker.string.uuid()
     };
 }
 
 function siteMetadata(metadata: Partial<SiteMetadata> = {}): SiteMetadata {
     return {
         title: faker.word.noun(),
-        clientId: faker.datatype.uuid(),
+        clientId: faker.string.uuid(),
         siteUrl: faker.internet.url(),
-        author: faker.name.fullName(),
+        author: faker.person.fullName(),
         description: faker.lorem.sentence(),
         ...metadata
     };
