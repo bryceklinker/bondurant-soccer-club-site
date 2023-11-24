ae24c2954a3b7332f4231a10f915b1c6ce684f08
Bryce Klinker <bryce.klinker@gmail.com>
2023-11-24 08:21:36 -0600

    upgrading terraform and azurerm provider

--- infra/modules/static_site/main.tf
+++ infra/modules/static_site/main.tf
@@ -35,7 +35,7 @@ locals {
 data "azurerm_client_config" "current" {}
 
 resource "azuread_service_principal" "cdn" {
-  application_id = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
+  client_id = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
   use_existing   = true
 }
 
